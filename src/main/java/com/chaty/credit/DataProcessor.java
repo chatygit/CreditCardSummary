4368ba17e843798ef598a3c7168a0e63a5848b71
chaty <kbabachaitanya@gmail.com>
2020-01-08 17:50:59 -0500

    cleanup and javadoc

--- src/main/java/com/chaty/credit/DataProcessor.java
+++ src/main/java/com/chaty/credit/DataProcessor.java
@@ -1,11 +1,8 @@
 package com.chaty.credit;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -17,17 +14,31 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
 
-import javax.xml.parsers.ParserConfigurationException;
-
 import org.apache.commons.lang3.StringUtils;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.text.PDFTextStripper;
-import org.apache.pdfbox.text.PDFTextStripperByArea;
-import org.fit.pdfdom.PDFDomTree;
 import org.springframework.stereotype.Component;
 
 import com.chaty.credit.model.CreditFileModel;
 
+/**
+ * 
+ * Loads different sources of data into a generic {@link CreditFileModel}
+ * objects.
+ * 
+ * Currently has support for CSV, with Date("MM/dd/yyyy"),location,debit,credit
+ * as columns.
+ * 
+ * Since Current Major Canadian Banks mostly have PDF, you can copy paste the
+ * content onto text files in the following format.
+ * 
+ * 
+ *  Debit : MAR 23 MAR 25 THE BEER STORE #2369 NORTH YORK $112.85 
+ *  Credit: MAR 25 MAR 26 PAYMENT - THANK YOU -$300.00
+ * 
+ * ```
+ * 
+ * @author chaty
+ *
+ */
 @Component
 public class DataProcessor {
 
@@ -80,49 +91,10 @@ public class DataProcessor {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		yearList.addAll(processTextile());
 
 		return yearList;
-
-	}
-
-	public void processPDFFile() {
-
-		try (PDDocument document = PDDocument.load(
-				new File("/Users/chaty/Documents/TaxDocs/OLD-VSA/2013/TD_REWARDS_VISA_CARD_5035_Apr_10-2013.pdf"))) {
-
-			document.getClass();
-
-			Writer output = new PrintWriter("/Users/chaty/Downloads/pdf.json");
-			new PDFDomTree().writeText(document, output);
-
-			output.close();
-
-			if (!document.isEncrypted()) {
-
-				PDFTextStripperByArea stripper = new PDFTextStripperByArea();
-				stripper.setSortByPosition(true);
-
-				PDFTextStripper tStripper = new PDFTextStripper();
-
-				String pdfFileInText = tStripper.getText(document);
-				System.out.println("Text:" + pdfFileInText);
-
-				// split by whitespace
-				String lines[] = pdfFileInText.split("\\r?\\n");
-				for (String line : lines) {
-					System.out.println(line);
-				}
-
-			} else {
-				System.out.println("ENCRYPTED");
-
-			}
-
-		} catch (IOException | ParserConfigurationException e) {
-			System.out.println(e);
-		}
 
 	}
 
@@ -164,12 +136,12 @@ public class DataProcessor {
 								.withDayOfMonth(Integer.parseInt(day));
 
 						CreditFileModel model = new CreditFileModel();
-						if(isCredit) {
-							model.setCredit(val.replaceAll(",", ""));	
-						}else {
+						if (isCredit) {
+							model.setCredit(val.replaceAll(",", ""));
+						} else {
 							model.setDebit(val.replaceAll(",", ""));
 						}
-						
+
 						model.setDate(date);
 						model.setLocation(line.substring(location, line.lastIndexOf("$")));
 						yearList.add(model);
--- src/main/java/com/chaty/credit/magic/AggregatePurchase.java
+++ src/main/java/com/chaty/credit/magic/AggregatePurchase.java
@@ -10,6 +10,17 @@ import org.springframework.stereotype.Component;
 import com.chaty.credit.model.CreditFileModel;
 import com.chaty.credit.model.TotalByLocation;
 
+
+
+
+/**
+ * 
+ * Does some basic stream filtering and aggregating for easier display of data
+ * by client facing applications.
+ * 
+ * @author chaty
+ *
+ */
 @Component
 public class AggregatePurchase {
 
@@ -21,65 +32,41 @@ public class AggregatePurchase {
 	 */
 	public List<TotalByLocation> aggregateByYear(final List<CreditFileModel> dataList) {
 
-		Map<Integer, List<CreditFileModel>> mappedDate = new ConcurrentHashMap<Integer, List<CreditFileModel>>();
+		final Map<String, List<CreditFileModel>> mappedDate = new ConcurrentHashMap<String, List<CreditFileModel>>();
 
 		dataList.stream().forEach(row -> {
 
-			if (mappedDate.containsKey(row.getDate().getYear())) {
+			if (mappedDate.containsKey(Integer.toString(row.getDate().getYear()))) {
 
-				List<CreditFileModel> itemList = mappedDate.get(row.getDate().getYear());
+				List<CreditFileModel> itemList = mappedDate.get(Integer.toString(row.getDate().getYear()));
 				if (row.getDate() != null) {
 					itemList.add(row);
-					mappedDate.put(row.getDate().getYear(), itemList);
+					mappedDate.put(Integer.toString(row.getDate().getYear()), itemList);
 				}
 
 			} else {
 				if (row.getDate() != null) {
 					List<CreditFileModel> itemList2 = new ArrayList<>();
 					itemList2.add(row);
-					mappedDate.put(row.getDate().getYear(), itemList2);
+					mappedDate.put(Integer.toString(row.getDate().getYear()), itemList2);
 				}
 			}
 
 		});
 
-		List<TotalByLocation> totalByLocation = new ArrayList<TotalByLocation>();
-
-		mappedDate.entrySet().stream().forEach(entry -> {
-			TotalByLocation totalLoc = new TotalByLocation();
-			totalLoc.setLocation(entry.getKey().toString());
-			totalLoc.setPurchaseList(entry.getValue());
-
-			Double totalCredit = entry.getValue().stream().mapToDouble(pp -> {
-				if (pp.getCredit() != null && !pp.getCredit().isEmpty()) {
-					return Double.parseDouble(pp.getCredit());
-				} else {
-					return new Double(0.00);
-				}
-
-			}).sum();
-
-			Double totalDebit = entry.getValue().stream().mapToDouble(pp -> {
-				if (pp.getDebit() != null && !pp.getDebit().isEmpty()) {
-					return Double.parseDouble(pp.getDebit());
-				} else {
-					return new Double(0.00);
-				}
-
-			}).sum();
-			totalLoc.setCreditTotal(totalCredit.intValue());
-			totalLoc.setDebitTotal(totalDebit.intValue());
-			totalByLocation.add(totalLoc);
-		});
-		totalByLocation.sort((a, b) -> a.getCreditTotal().compareTo(b.getCreditTotal()));
-
-		return totalByLocation;
+		return aggregateTotals(mappedDate);
 
 	}
 
+	/**
+	 * Aggregates given list by Location.
+	 * 
+	 * @param dataList
+	 * @return List of {@link TotalByLocation}
+	 */
 	public List<TotalByLocation> aggregateByLocation(final List<CreditFileModel> dataList) {
 
-		Map<String, List<CreditFileModel>> maptoLocation = new ConcurrentHashMap<String, List<CreditFileModel>>();
+		final Map<String, List<CreditFileModel>> maptoLocation = new ConcurrentHashMap<String, List<CreditFileModel>>();
 
 		dataList.stream().forEach(row ->
 
@@ -96,6 +83,18 @@ public class AggregatePurchase {
 			}
 
 		});
+
+		return aggregateTotals(maptoLocation);
+
+	}
+
+	/**
+	 * Sums all the Credit and Debt values.
+	 * 
+	 * @param maptoLocation
+	 * @return List of {@link TotalByLocation}
+	 */
+	private List<TotalByLocation> aggregateTotals(final Map<String, List<CreditFileModel>> maptoLocation) {
 
 		List<TotalByLocation> totalByLocation = new ArrayList<TotalByLocation>();
 
